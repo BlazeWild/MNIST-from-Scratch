ce6414e5a4fb2655cb1e5279421033bba15defc6
Ashok Bk <ashokbk215@gmail.com>
2024-06-16 13:07:46 +0545

    Added

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import numpy as np
-x = np.random.rand(2)
+x = np.random.rand(2,5)
 print(x) 
 
 np.random.seed(42)
-y = np.random.rand(2)
+y = np.random.randn(2,5)
 print(y)
 #you can see how the value of x changes every time the code runs, but y doesn't change
